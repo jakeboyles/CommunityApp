5bd5fa01443e4e2c7196acbe336b357d16175b78
Jake Boyles <jake.boyles@docustar.com>
2014-06-05 16:04:48 -0400

    fixes

--- server.js
+++ server.js
@@ -14,8 +14,25 @@ require('./config/passport')(passport); // pass passport for configuration
 
 
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+
 
 app.configure(function() {
+	app.use(allowCrossDomain);
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
