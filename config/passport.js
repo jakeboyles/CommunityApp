eb59583f9a3384633f1a06a023adf47f1ff0175a
Jake Boyles <jake.boyles@docustar.com>
2014-06-05 16:38:21 -0400

    fixes

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@
 
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
+var BasicStrategy = require('passport-http').BasicStrategy;
 
 // load up the user model
 var User       		= require('../app/models/user');
@@ -29,15 +30,15 @@ module.exports = function(passport) {
 
 
     passport.use(new BasicStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.validPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
+      function(email, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!user.validPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });
+      }
+    ));
 
  	// =========================================================================
     // LOCAL SIGNUP ============================================================
