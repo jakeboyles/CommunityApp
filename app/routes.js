9a5eb2e07a11b654dd1897c90160486c7053539d
Jake Boyles <jake.boyles@docustar.com>
2014-06-05 15:48:21 -0400

    fix

--- app/routes.js
+++ app/routes.js
@@ -3,14 +3,16 @@ var User = require('./models/user');
 var Comment = require('./models/comment');
 
 
-app.all('/', function(req, res, next) {
+
+
+
+module.exports = function(app,passport) {
+
+	app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
-
-
-module.exports = function(app,passport) {
 
 	// api ---------------------------------------------------------------------
 	// get all todos
