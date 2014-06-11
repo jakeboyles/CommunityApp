e463b02b6e95cf4daaace5397c008a19643afead
Jake Boyles <jake@infotrustllc.com>
2014-06-10 23:21:57 -0400

    fixes

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ var flash 	 = require('connect-flash');
 // configuration ===============================================================
 
 
-mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/testMongo';
-
-
-
-mongoose.connect(mongoUri); 	// connect to mongoDB database on modulus.io
+mongoose.connect("mongodb://jake@jibdesigns.com:Baseball_200@kahana.mongohq.com:10097/app26238411"); 	// connect to mongoDB database on modulus.io
 
 require('./config/passport')(passport); // pass passport for configuration
 
