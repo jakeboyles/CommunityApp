a23cf2ece01318d04157fcf4d760309268dbb21d
Jake Boyles <jake.boyles@docustar.com>
2014-06-12 13:42:28 -0400

    fixes

--- public/js/controllers/getPost.js
+++ public/js/controllers/getPost.js
@@ -67,8 +67,8 @@ angular.module('SinglePostController', [])
 
 
 		$scope.createComment = function(){
-			alert("A:"+$scope.comment.offer);
-			if(!isNaN($scope.comment.offer) || typeof $scope.comment.offer==undefined || $scope.comment.offer=="") {
+			alert("B:"+$scope.comment.offer);
+			if(!isNaN($scope.comment.offer) || $scope.comment.offer===undefined || $scope.comment.offer=="") {
 			$scope.comment.postid = $routeParams.postid;
 			Communities.postComment($scope.comment)
 			.success(function(data){
