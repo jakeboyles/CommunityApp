328b9db549978aac1052d967f555357930d08974
Jake Boyles <jake.boyles@docustar.com>
2014-06-12 13:18:18 -0400

    fixes

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ require('./config/passport')(passport); // pass passport for configuration
 //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
 
 
  
